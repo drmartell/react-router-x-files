2d0a4e4c3f79912398ceb938ab1b71a33aa8515c
Casey Martell <drmartell@gmail.com>
2020-02-06 09:40:36 -0800

    refactor details destructuring

--- src/services/getCharacterByName.js
+++ src/services/getCharacterByName.js
@@ -2,11 +2,9 @@ export default name =>
   //fetch(`https://cors-anywhere.herokuapp.com/https://xfiles-api.herokuapp.com/api/v1/characters/${encodeURIComponent(name)}`)
   fetch(`https://xfiles-api.herokuapp.com/api/v1/characters/${encodeURIComponent(name)}`)
     .then(res => res.json())
-    .then(characterArr => {
-      const { name, image, description } = characterArr[0];
-      return ({
+    .then(
+      ([{ name, image, description }]) => ({
         name,
         image,
         description
-      });
-    });
+      }));
