83238e2125dc9103b827d03b4e77ef06cc58d98e
Casey Martell <drmartell@gmail.com>
2020-02-05 16:25:19 -0800

    adds components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+import Details from './Details';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/:name" component={Details} />
+      </Switch>
+    </Router>
+  );
 }
-  
---
+++ src/components/Details.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Details() {
+  return <p>This is where the details will go</p>;
+}
